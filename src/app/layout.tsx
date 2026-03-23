8604945366ed0b0cda522f238d02a5e7138ca2e5
mide2020-16 <efuwapeayomide51@gmail.com>
2026-03-23 14:48:41 +0100

    analytics added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata, Viewport } from 'next';
+import { Analytics } from "@vercel/analytics/next"
 import { Geist } from 'next/font/google';
 import './globals.css';
 
@@ -56,6 +57,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${geistSans.variable} antialiased`}>
         {children}
+        <Analytics />
       </body>
     </html>
   );
